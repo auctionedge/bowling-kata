703ff2564fbbca45e804ed9e30ba10d8cc3bf90b
Doug <dough@ptera.net>
2018-09-16 19:03:08 -0700

    First actual changes.

--- src/main/java/com/auctionedge/BowlingGame.java
+++ src/main/java/com/auctionedge/BowlingGame.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class BowlingGame {
 
+	private int score = 0;
+
 	public void rolls(List<Integer> theRolls) {
+	    score = 0;
+        for (Integer roll : theRolls) {
+            System.out.format("roll: %d\n", roll);
+            score += roll;
+        }
 	}
 
 	public int score() {
-		return 0;
+		return score;
 	}
 }
